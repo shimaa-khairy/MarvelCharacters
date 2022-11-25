3b3bef3b25c174350c39db618ea5f17e28e0f510
shimaa khairy <shkhairy@2p.com.sa>
2022-11-25 16:52:48 +0200

    init app and pods

---
+++ MaravelCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaravelCharacters
+//
+//  Created by shimaa khairy on 25/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
